50465bf2c99c576449918f12a0c606cf0bde466a
cristian-castro-a <cr.castro.alvarez@gmail.com>
2022-09-07 20:22:25 +0200

    Update

---
+++ Bash.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+conda create -n CO2_emissions python=3.7 ipython ipykernel
+
+conda activate CO2_emissions
+
+python -m ipykernel install --user --name=CO2_emissions
+
+conda install pandas
+conda install -c anaconda scikit-learn
+conda install -c conda-forge matplotlib
+conda install -c anaconda tensorflow
